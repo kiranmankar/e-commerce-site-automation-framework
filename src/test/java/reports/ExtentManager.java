dacd05a5c353fe2cd842ca0043daa126a267d301
Kiran <kiranmankar357@gmail.com>
2026-06-07 19:34:04 +0530

    add ExtentManager and fix gitignore

---
+++ src/test/java/reports/ExtentManager.java
@@ -0,0 +1,33 @@
+package reports;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+	
+	private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+
+        if (extent == null) {
+
+            ExtentSparkReporter spark =
+                    new ExtentSparkReporter(
+                            "reports/ExtentReport.html");
+
+            spark.config()
+                    .setReportName(
+                            "SauceDemo Automation Report");
+
+            spark.config()
+                    .setDocumentTitle(
+                            "Execution Report");
+
+            extent = new ExtentReports();
+
+            extent.attachReporter(spark);
+        }
+
+        return extent;
+    }
+}
